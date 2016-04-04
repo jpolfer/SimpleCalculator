21d43f2cf57230a31f27fe81fb08315847509da0
jpolfer <jonathan.polfer@gmail.com>
2016-04-04 12:11:41 -0500

    Initial commit of Simple Calculator shell

---
+++ SimpleCalculator/SimpleCalculator/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SimpleCalculator.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleCalculator.Startup))]
+namespace SimpleCalculator
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
