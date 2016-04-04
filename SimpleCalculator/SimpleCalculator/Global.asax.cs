a52d4ee89fe8d3c1b8818bf545aadc03ea54adfb
jpolfer <jonathan.polfer@gmail.com>
2016-04-04 16:01:11 -0500

    Added database initialization.

--- SimpleCalculator/SimpleCalculator/Global.asax.cs
+++ SimpleCalculator/SimpleCalculator/Global.asax.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Data.SQLite;
 
 namespace SimpleCalculator
 {
@@ -16,6 +18,20 @@ namespace SimpleCalculator
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            string dbfilename = "C:\\tmp\\simplecalculator.sqlite";
+            if(!File.Exists(dbfilename))
+            {
+                // Create db
+                SQLiteConnection.CreateFile(dbfilename);
+
+                // Create history table
+                SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + dbfilename + ";Version=3;");
+                m_dbConnection.Open();
+                string createHistoryTableSql = "CREATE TABLE history (timestamp TEXT, command TEXT);";
+                SQLiteCommand createHistoryTable = new SQLiteCommand(createHistoryTableSql, m_dbConnection);
+                createHistoryTable.ExecuteNonQuery();
+            }
         }
     }
 }
