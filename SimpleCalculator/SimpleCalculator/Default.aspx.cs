412af14b76897cc4a407909b7ecd65d920e442be
jpolfer <jonathan.polfer@gmail.com>
2016-04-05 10:29:57 -0500

    Made evaluation more robust

--- SimpleCalculator/SimpleCalculator/Default.aspx.cs
+++ SimpleCalculator/SimpleCalculator/Default.aspx.cs
@@ -14,10 +14,26 @@ namespace SimpleCalculator
         {
             if(Page.IsPostBack)
             {
-                // Evaluate statement
-                object commandResult = CsEval.Eval(this, txtThingToCalculate.Text);
-                lblResult.Text = commandResult.ToString();
+                if(!string.IsNullOrWhiteSpace(txtThingToCalculate.Text))
+                {
+                    // Evaluate statement
+                    try
+                    {
+                        string result = EvaluateStatement(txtThingToCalculate.Text);
+                        lblResult.Text = result;
+                    }
+                    catch (Exception ex)
+                    {
+                        lblResult.Text = "An Error has occurred during evaluation.  Please try again!";
+                    }
+                }
             }
+        }
+
+        private string EvaluateStatement(string statementToEvaluate)
+        {
+                object commandResult = CsEval.Eval(this, statementToEvaluate);
+                return commandResult.ToString();
         }
     }
 }
