827c92ab4c5df0227252d3a8d455d9f3a6cfc315
jpolfer <jonathan.polfer@gmail.com>
2016-04-11 09:14:58 -0500

    Added db context for SimpleCalculator

---
+++ SimpleCalculator/SimpleCalculator/SimpleCalculatorContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SimpleCalculator
+{
+    public class SimpleCalculatorContext : DbContext
+    {
+        public DbSet<HistoryEntry> HistoryEntries { get; set; }
+    }
+}
