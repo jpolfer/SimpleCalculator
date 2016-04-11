548b13926c454fb41aced2d9788d967c57f71df6
jpolfer <jonathan.polfer@gmail.com>
2016-04-11 09:40:25 -0500

    Got EF working with code.

--- SimpleCalculator/SimpleCalculator/DatabaseHelper.cs
+++ SimpleCalculator/SimpleCalculator/DatabaseHelper.cs
@@ -10,81 +10,53 @@ namespace SimpleCalculator
 {
     public static class DatabaseHelper
     {
-        public static string dbfilename = "C:\\tmp\\simplecalculator.sqlite";
-
         public static void SetupDatabase()
         {
-            if(!File.Exists(DatabaseHelper.dbfilename))
-            {
-                // Create db
-                SQLiteConnection.CreateFile(DatabaseHelper.dbfilename);
-
-                // Create history table
-                using (SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + DatabaseHelper.dbfilename + ";Version=3;"))
-                {
-                    m_dbConnection.Open();
-                    string createHistoryTableSql = "CREATE TABLE history (timestamp TEXT, command TEXT);";
-                    using (SQLiteCommand createHistoryTable = new SQLiteCommand(createHistoryTableSql, m_dbConnection))
-                    {
-                        createHistoryTable.ExecuteNonQuery();
-                    }
-                }
-            }
+            // no longer used
         }
 
         public static List<HistoryEntry> GetHistoryEntriesFromDb()
         {
-            List<HistoryEntry> historyEntries = new List<HistoryEntry>();
-            using (SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + DatabaseHelper.dbfilename + ";Version=3;"))
+            using (var db = new SimpleCalculatorContext())
             {
-                string sql = "select timestamp, command from history";
-                using (SQLiteCommand sqlCommand = new SQLiteCommand(sql, m_dbConnection))
-                {
-                    m_dbConnection.Open();
-                    using (SQLiteDataReader reader = sqlCommand.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            HistoryEntry newEntry = new HistoryEntry()
-                            {
-                                Timestamp = reader["timestamp"].ToString(),
-                                Command = reader["command"].ToString()
-                            };
-                            historyEntries.Add(newEntry);
-                        }
-                    }
-                }
+                List<HistoryEntry> historyEntries = db.HistoryEntries
+                                                      .ToArray<HistoryEntry>()
+                                                      .OrderBy(x => Convert.ToDateTime(x.Timestamp))
+                                                      .ToList<HistoryEntry>();
+                return historyEntries;
             }
-
-            return historyEntries;
+            return new List<HistoryEntry>();
         }
 
         public static void ClearHistoryTable()
         {
-            using (SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + DatabaseHelper.dbfilename + ";Version=3;"))
+            using (var db = new SimpleCalculatorContext())
             {
-                m_dbConnection.Open();
-                string sql = "delete from history";
-                using (SQLiteCommand sqlCommand = new SQLiteCommand(sql, m_dbConnection))
+                foreach(HistoryEntry entry in db.HistoryEntries)
                 {
-                    sqlCommand.ExecuteNonQuery();
+                    db.HistoryEntries.Remove(entry);
                 }
+                db.SaveChanges();
             }
         }
 
         public static bool StoreEvaluationToHistoryTable(string command, string result)
         {
-            using (SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + DatabaseHelper.dbfilename + ";Version=3;"))
+            using (var db = new SimpleCalculatorContext())
             {
-                m_dbConnection.Open();
-                string timestamp = DateTime.Now.ToString();
-                string evaluationStatement = command + " = " + result;
-                string sql = "insert into history (timestamp, command) values ('" + timestamp + "', '" + evaluationStatement + "')";
-                using (SQLiteCommand sqlCommand = new SQLiteCommand(sql, m_dbConnection))
+                try
                 {
-                    int sqlCommandResult = sqlCommand.ExecuteNonQuery();
-
-                    return sqlCommandResult == 1;
+                    HistoryEntry newEntry = new HistoryEntry()
+                    { Command = command + " = " + result,
+                      Timestamp = DateTime.Now.ToString()
+                    };
+                    db.HistoryEntries.Add(newEntry);
+                    db.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
                 }
             }
         }
--- SimpleCalculator/SimpleCalculator/HistoryEntry.cs
+++ SimpleCalculator/SimpleCalculator/HistoryEntry.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SimpleCalculator
 {
     public class HistoryEntry
     {
+        [Key]
         public string Timestamp { get; set; }
+
         public string Command { get; set; }
     }
 }
