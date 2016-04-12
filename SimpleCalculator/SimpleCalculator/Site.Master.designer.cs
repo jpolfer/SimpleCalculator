695939fe61f49740f0120f97f57f6f4b1e99ed22
jpolfer <jonathan.polfer@gmail.com>
2016-04-12 16:52:28 -0500

    Added headercontent

--- SimpleCalculator/SimpleCalculator/Site.Master.designer.cs
+++ SimpleCalculator/SimpleCalculator/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace SimpleCalculator {
     public partial class SiteMaster {
         
         /// <summary>
+        /// HeaderStyles control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeaderStyles;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
